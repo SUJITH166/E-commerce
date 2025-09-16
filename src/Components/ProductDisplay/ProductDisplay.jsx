e714d7167a0461360cb84bee83e091463124a3c7
Sujith S <sujithsree009@gmail.com>
2025-09-16 12:11:06 +0530

    Added product page

---
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import star_icon from'../Assets/star_icon.png';
+import star_dull_icon from '../Assets/star_dull_icon.png'
+const ProductDisplay = (props) => {
+  const { product } = props;
+  return (
+    <div className="productDisplay">
+      <div className="productdisplat-left">
+        <div className="productdisplay-img-list">
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+        </div>
+        <div className="productdisplay-img">
+            <img className="productdisplay-main-img" src={product.image}/>
+        </div>
+      </div>
+      <div className="productdisplay-right">
+        <h1>{product.name}</h1>
+        <div className="productdisplay-right-star">
+            <img src="" alt=""/>
+            <img src="" alt=""/>
+            <img src="" alt=""/>
+            <img src="" alt=""/>
+            <img src="" alt=""/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDisplay;
