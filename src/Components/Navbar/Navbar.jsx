ad7ba42a4e1d794e3b4bf68de442aa6cde3e63bc
Sujith S <sujithsree009@gmail.com>
2025-10-06 17:38:09 +0530

    admin panel

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useContext, useRef, useState } from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
@@ -78,3 +79,20 @@ const Navbar = () => {
 };
 
 export default Navbar;
+=======
+import React from 'react'
+import './Navbar.css'
+import navlogo from '../../assets/nav-logo.svg';
+import navProfile from '../../assets/nav-profile.svg'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={navlogo} alt='' className='nav-logo'></img>
+      <img src={navProfile} alt='' className='nav-profile'/>
+    </div>
+  )
+}
+
+export default Navbar
+>>>>>>> 52f9549 (admin panel)
